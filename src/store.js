829b8a0c803307f2f31b0a0a348045e54ad79b3a
ablakerose <bdecktor@gmail.com>
2020-03-03 15:51:24 -0500

    create reducer to combine reducers

--- src/store.js
+++ src/store.js
@@ -1,12 +1,17 @@
 import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import thunk from "redux-thunk";
-import usersReducer from "./reducers/users";
+import usersReducer from "./reducers/users.js";
+import currentUser from "./reducers/currentUser.js";
+import loginForm from "./reducers/loginForm.js";
 
+//just displying different syntax options here (could name the state something else)
 const reducer = combineReducers({
-  user: usersReducer
+  usersReducer,
+  currentUser,
+  loginForm
 });
 
-const composeEnhancer = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)));
 
