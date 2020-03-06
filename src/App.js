0737a5a2040cac6879d6a7818849c0e64be30222
ablakerose <bdecktor@gmail.com>
2020-03-06 09:56:22 -0500

    add route and wrap div in router

--- src/App.js
+++ src/App.js
@@ -4,19 +4,31 @@ import { connect } from "react-redux";
 import { getCurrentUser } from "./actions/currentUser.js";
 import NavBar from "./components/NavBar.js";
 import MainContainer from "./components/MainContainer.js";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Login from "./components/Login.js";
+import Signup from "./components/Signup.js";
+import MyNeeds from "./components/MyNeeds.js";
 
 class App extends React.Component {
   componenetDidMount() {
-    this.props.getCurrentUser();
+    //  this.props.getCurrentUser();
+    //getCurrentUser for authorization than authentication.
+    //can call it wherever i need a reference to the currentUser
   }
   render() {
     return (
-      <div className="App">
-        <NavBar />
-        <MainContainer />
-      </div>
+      <Router>
+        <div className="App">
+          <Logout />
+          <NavBar />
+
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/ my-needs" component={MyNeeds} />
+        </div>
+      </Router>
     );
   }
 }
 
-export default App;
+export default connect(null, { getCurrentUser })(App);
